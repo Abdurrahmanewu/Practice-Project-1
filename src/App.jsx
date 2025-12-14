d42cc66725a52fd9c7e2618d08db42186c56ff9d
Abdurrahmanewu <abdurrahman.ewu.bd@gmail.com>
2025-12-14 12:31:09 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// import "./App.css";
+
+import Navbar from "./Components/Navbar/Navbar";
+
+function App() {
+  return (
+    <>
+      <div className="bg-gray-100 min-h-screen flex flex-col">
+        <Navbar />
+      </div>
+    </>
+  );
+}
+
+export default App;
