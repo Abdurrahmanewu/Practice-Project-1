3f55268abea262665f208ca589f7e0de3d7870bd
Abdurrahmanewu <abdurrahman.ewu.bd@gmail.com>
2025-12-14 13:10:44 +0600

    nav done

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 // import "./App.css";
 
+import Button from "./Components/Buttons/Button";
 import Navbar from "./Components/Navbar/Navbar";
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <>
       <div className="bg-gray-100 min-h-screen flex flex-col">
         <Navbar />
+        
       </div>
     </>
   );
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,4 +1,5 @@
 import logo from "../../assets/logo.svg";
+import Button from "../Buttons/Button";
 
 const Navbar = () => {
   return (
@@ -14,13 +15,9 @@ const Navbar = () => {
           </span>
         </a>
         <div className="flex items-center lg:order-2">
-          <a
-            href="#"
-            target="_blank"
-            className="text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 sm:mr-2 lg:mr-0 dark:bg-purple-600 dark:hover:bg-purple-700 focus:outline-none dark:focus:ring-purple-800"
-          >
+          <Button className="text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 sm:mr-2 lg:mr-0 dark:bg-purple-600 dark:hover:bg-purple-700 focus:outline-none dark:focus:ring-purple-800">
             Download
-          </a>
+          </Button>
         </div>
         <div
           className="items-center justify-between hidden w-full lg:flex lg:w-auto lg:order-1"
