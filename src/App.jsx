ef3fa8d024ad344cc77057268bbaf54b93fa727c
Abdurrahmanewu <abdurrahman.ewu.bd@gmail.com>
2025-12-14 15:25:57 +0600

    hero done

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 // import "./App.css";
 
 import Button from "./Components/Buttons/Button";
+import HeroSection from "./Components/HeroSection/HeroSection";
 import Navbar from "./Components/Navbar/Navbar";
 
 function App() {
@@ -8,7 +9,7 @@ function App() {
     <>
       <div className="bg-gray-100 min-h-screen flex flex-col">
         <Navbar />
-        
+        <HeroSection />
       </div>
     </>
   );
