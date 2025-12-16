a15d84a0f198335c715fc5f88485b52e224cd004
Abdurrahmanewu <abdurrahman.ewu.bd@gmail.com>
2025-12-16 12:13:02 +0600

    logo updated

--- src/Components/CompaniesLogo/CompaniesLogo.jsx
+++ src/Components/CompaniesLogo/CompaniesLogo.jsx
@@ -1,10 +1,4 @@
-import React from "react";
-import AirBNB from "./AllLogo/AirBNB";
-import Google from "./AllLogo/Google";
-import Microsoft from "./AllLogo/Microsoft";
-import Spotify from "./AllLogo/Spotify";
-import Mailchimp from "./AllLogo/Mailchimp";
-import Mashable from "./AllLogo/Mashable";
+import SingleLogocard from "./AllLogo/SingleLogocard";
 
 const CompaniesLogo = () => {
   return (
@@ -12,23 +6,34 @@ const CompaniesLogo = () => {
       <div className="max-w-7xl px-4 pb-8 mx-auto lg:pb-16">
         <div className="grid grid-cols-2 gap-8 text-gray-500 sm:gap-12 sm:grid-cols-3 lg:grid-cols-6 dark:text-gray-400">
           <a className="flex items-center lg:justify-center">
-            <AirBNB className="h-9 hover:text-gray-900 dark:hover:text-white" />
+            <SingleLogocard name="AirBNB" viewBox="0 0 125 35"></SingleLogocard>
           </a>
           <a className="flex items-center lg:justify-center">
-            <Google className="h-9 hover:text-gray-900 dark:hover:text-white" />
+            <SingleLogocard name="Google" viewBox="0 0 86 29"></SingleLogocard>
           </a>
           <a className="flex items-center lg:justify-center">
-            <Mailchimp className="h-9 hover:text-gray-900 dark:hover:text-white" />
+            <SingleLogocard
+              name="Microsoft"
+              viewBox="0 0 151 34"
+            ></SingleLogocard>
           </a>
           <a className="flex items-center lg:justify-center">
-            <Microsoft className="h-9 hover:text-gray-900 dark:hover:text-white" />
+            <SingleLogocard
+              name="Spotify"
+              viewBox="0 0 124 38"
+            ></SingleLogocard>
           </a>
           <a className="flex items-center lg:justify-center">
-            <Spotify className="h-9 hover:text-gray-900 dark:hover:text-white" />
+            <SingleLogocard
+              name="Mailchim"
+              viewBox="0 0 137 37"
+            ></SingleLogocard>
           </a>
-
           <a className="flex items-center lg:justify-center">
-            <Mashable className="h-9 hover:text-gray-900 dark:hover:text-white" />
+            <SingleLogocard
+              name="Mashable"
+              viewBox="0 0 124 21"
+            ></SingleLogocard>
           </a>
         </div>
       </div>
